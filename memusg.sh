e51d87cddcc22dfa39a52203ac62b36f0b91adac
Amin Allam <amin.allam@kaust.edu.sa>
2014-03-28 18:11:50 +0300

    Update memusg.sh

--- memusg.sh
+++ memusg.sh
@@ -29,7 +29,7 @@
 # The peak memory usage of each process will be save in a file inside the folder ./memusg
 # The file name will be: processName_processId_processStartTime
 # The file will contain two lines: first line contains memory usage in MBs, second line contains parent process ID, and the command which launched the process
-# The script takes 4 parameters:
+# The script takes 5 parameters:
 #   1) Time interval (in seconds) between consecutive checks
 #   2) Minimum memory (in MBs) size to track (avoid tracking any process whose memory peak does not exceed this value)
 #   3) Flag to empty the ./memusg folder before starting ("clean" or "noclean")
